5ae5babb31638c6623f6d89cc13a6eab446dd1d6
leandro.coelho <leandro.coelho@ti34.metasolutions.local>
2016-09-28 17:14:36 -0300

    Ajustes para incluir tela de login, os grupos e permissoes.

--- algaworks/brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
+++ algaworks/brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.helper.usuarios.UsuariosQueries;
 
-public interface Usuarios extends JpaRepository<Usuario, Long> {
+public interface Usuarios extends JpaRepository<Usuario, Long>, UsuariosQueries {
 
 	public Optional<Usuario> findByEmailIgnoreCase(String email);
+
 }
