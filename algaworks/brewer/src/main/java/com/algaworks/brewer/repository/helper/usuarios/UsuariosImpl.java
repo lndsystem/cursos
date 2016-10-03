fff1a6702353eada3d438e362f33d2e7cfafb07d
lndsystem <leandroces@gmail.com>
2016-10-03 06:49:09 -0300

    Ajustes para a aula 21.3

--- algaworks/brewer/src/main/java/com/algaworks/brewer/repository/helper/usuarios/UsuariosImpl.java
+++ algaworks/brewer/src/main/java/com/algaworks/brewer/repository/helper/usuarios/UsuariosImpl.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository.helper.usuarios;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,9 +9,13 @@ import javax.persistence.PersistenceContext;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
+import org.hibernate.sql.JoinType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -18,7 +23,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.algaworks.brewer.model.Grupo;
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.model.UsuarioGrupo;
 import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 
@@ -50,6 +57,7 @@ public class UsuariosImpl implements UsuariosQueries {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Usuario.class);
 
 		paginacaoUtil.preparar(criteria, pageable);
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 		adicionarFiltro(filtro, criteria);
 
 		return new PageImpl<>(criteria.list(), pageable, total(filtro));
@@ -67,7 +75,24 @@ public class UsuariosImpl implements UsuariosQueries {
 			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
 		}
 		if (!StringUtils.isEmpty(filtro.getEmail())) {
-			criteria.add(Restrictions.ilike("email", filtro.getEmail(), MatchMode.ANYWHERE));
+			criteria.add(Restrictions.ilike("email", filtro.getEmail(), MatchMode.START));
+		}
+
+		criteria.createAlias("grupos", "g", JoinType.LEFT_OUTER_JOIN);
+		
+		if (filtro.getGrupos() != null && !filtro.getGrupos().isEmpty()) {
+			List<Criterion> subqueries = new ArrayList<>();
+			
+			for(Long codigoGrupo : filtro.getGrupos().stream().mapToLong(Grupo::getCodigo).toArray()){
+				DetachedCriteria dc = DetachedCriteria.forClass(UsuarioGrupo.class);
+				dc.add(Restrictions.eq("id.grupo.codigo", codigoGrupo));
+				dc.setProjection(Projections.property("id.usuario"));
+				
+				subqueries.add(Subqueries.propertyIn("codigo", dc));
+			}
+			
+			Criterion[] criterions = new Criterion[subqueries.size()];
+			criteria.add(Restrictions.and(subqueries.toArray(criterions)));
 		}
 	}
 }
