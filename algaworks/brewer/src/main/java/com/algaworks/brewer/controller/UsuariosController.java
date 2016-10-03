39c2dfdfada4bb61bab821594f91db5690abaa17
lndsystem <leandroces@gmail.com>
2016-10-03 06:01:52 -0300

    Ajustes feitos na aula 21.2, desafio para a tela de pesquisa de usuários

--- algaworks/brewer/src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ algaworks/brewer/src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -1,8 +1,12 @@
 package com.algaworks.brewer.controller;
 
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Grupos;
+import com.algaworks.brewer.repository.Usuarios;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.service.CadastroUsuarioService;
 import com.algaworks.brewer.service.exception.EmailUsuarioJaCadastradoException;
 import com.algaworks.brewer.service.exception.SenhaObrigatoriaUsuarioException;
@@ -24,6 +31,9 @@ public class UsuariosController {
 
 	@Autowired
 	private CadastroUsuarioService cadastroUsuarioService;
+	
+	@Autowired
+	private Usuarios usuarios;
 
 	@Autowired
 	private Grupos grupos;
@@ -53,6 +63,19 @@ public class UsuariosController {
 
 		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
 		return new ModelAndView("redirect:/usuarios/novo");
+	}
+
+	@RequestMapping
+	public ModelAndView pesquisa(UsuarioFilter usuarioFilter, BindingResult result,
+			@PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("usuario/PesquisaUsuario");
+
+		PageWrapper<Usuario> pageWrapper = new PageWrapper<>(usuarios.filtrar(usuarioFilter, pageable), httpServletRequest);
+		
+		mv.addObject("grupos", grupos.findAll());
+		mv.addObject("pagina", pageWrapper);
+		
+		return mv;
 	}
 
 }
