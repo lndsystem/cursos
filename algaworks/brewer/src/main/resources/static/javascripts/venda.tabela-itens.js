7a6906b4e50aaafa790afd959f8a606885550cd3
leandro.coelho <leandro.coelho@ti34.metasolutions.local>
2016-10-19 17:11:12 -0200

    Ajustes da aula 23.5

--- algaworks/brewer/src/main/resources/static/javascripts/venda.tabela-itens.js
+++ algaworks/brewer/src/main/resources/static/javascripts/venda.tabela-itens.js
@@ -1,10 +1,25 @@
 Brewer.TabelaItens = (function() {
 	function TabelaItens(autocomplete) {
-		this.autocomplite = autocomplete;
+		this.autocomplete = autocomplete;
 	}
 	
 	TabelaItens.prototype.iniciar = function() {
+		this.autocomplete.on('item-selecionado', onItemSelecionado.bind(this));
+	}
+	
+	function onItemSelecionado(event, item) {
+		var resposta = $.ajax({
+			url: 'item',
+			method: 'POST',
+			data: {
+				codigoCerveja: item.codigo
+			}
+		});
 		
+		resposta.done(function(data) {
+			console.log('retorno', data);
+		});
+		//console.log('Item recebido do autocomplete', item);
 	}
 	
 	return TabelaItens;
