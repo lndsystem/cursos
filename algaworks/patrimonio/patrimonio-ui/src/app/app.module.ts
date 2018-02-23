96c392e081b6015b98b7ff643f290caa25269211
Leandro Coelho Silva <leandro@MacBook-Pro-de-Leandro-2.local>
2018-02-23 06:36:02 -0300

    add

---
+++ algaworks/patrimonio/patrimonio-ui/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+
+import { DataTableModule } from 'primeng/primeng';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { ItemService } from './item.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    DataTableModule
+
+  ],
+  providers: [
+    ItemService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ algaworks/patrimonio/patrimonio-ui/src/app/item-cadastro/item-cadastro.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ItemService } from '../item.service';
+
+@Component({
+  selector: 'app-item-cadastro',
+  templateUrl: './item-cadastro.component.html',
+  styleUrls: ['./item-cadastro.component.css']
+})
+export class ItemCadastroComponent implements OnInit {
+
+  itens: any[] = [];
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.itemService.listar().subscribe(dados => this.itens = dados);
+  }
+
+}
