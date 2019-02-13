c347bd64b155949ae9a415216ae3752deab258ed
Leandro Coelho e Silva <leandroces@gmail.com>
2019-02-13 09:19:44 -0200

    add aula de arraw function e promises

--- algaworks/typescripts/3/ES6/arraw-functions.ts
+++ algaworks/typescripts/3/ES6/arraw-functions.ts
@@ -7,5 +7,17 @@ const precosEmReaisArrow = precos.map((preco) => {
     return 'R$ ' + preco + ',00';
 });
 
+const t1 = precos.map((preco) =>  'R$ ' + preco + ',00' );
+const t2 = precos.map((preco) =>  `R$ ${preco},00` );
+const t3 = precos.filter(preco => preco > '20').map(preco => `R$ ${preco},00`);
+
+console.log('tradicional');
 console.log(precosEmReais);
+console.log('arraw functions 1');
 console.log(precosEmReaisArrow);
+console.log('arraw functions 2');
+console.log(t1);
+console.log('arraw functions 3');
+console.log(t2);
+console.log('arraw functions 4');
+console.log(t3);
